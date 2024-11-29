1f396eabb65d278d397d13195c2d42ca3dfac2e7
trungtuan200304 <tran.trung.tuan.200304@gmail.com>
2024-11-29 14:58:58 +0700

    change route's name

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import router from './routes/userRoute.js';
+import userRoute from './routes/userRoute.js';
 import bodyParser from 'body-parser';
 dotenv.config();
 
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}))
-app.use("/users", router);
+app.use("/users", userRoute);
 
 
 
