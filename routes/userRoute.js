083119ae4de6e508272876fcc5399557841130d3
trungtuan200304 <tran.trung.tuan.200304@gmail.com>
2024-11-29 15:03:16 +0700

    add method get user by id

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,5 +7,17 @@ userRoute.get('/', (req, res) => {
     res.status(200).json(userData);
 });
 
+// Get user by id 
+userRoute.get('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const user = userData.find((user) => user.id === id);
+
+    if (!user) {
+        return res.status(404).json({ message: "user not found"});
+    }
+
+    res.status(200).json(user);
+})
+
 
 export default userRoute;
